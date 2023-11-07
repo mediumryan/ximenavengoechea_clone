5fbcd868791404e57c2562da979021786db2a590
Ryan <straightwind0726@gmail.com>
2023-11-07 13:02:06 +0900

    [Update] Installed essential libraries.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { styled } from 'styled-components';
 import './CSS/index.css';
 
+const MainOuter = styled.main`
+    padding: 10rem 20rem;
+`;
+
+const MainInner = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
 function App() {
-    return <div>hello, world</div>;
+    return (
+        <MainOuter>
+            <MainInner>hi</MainInner>
+        </MainOuter>
+    );
 }
 
 export default App;
